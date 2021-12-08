4b6408aa81cac6e224fa99fbd6c53815652fd588
unknown <raniaboudja4@gmail.com>
2021-12-08 00:48:04 +0000

    first push

---
+++ FileHelper.cs
@@ -0,0 +1,86 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+class FileHelper
+{
+
+    public static string ReadFile(string path)
+    {
+        string file = File.ReadAllText(path);
+        return file.Trim();
+    }
+    public static void WriteFile(string path, string content)
+    {
+        File.WriteAllText(path, content);
+    }
+
+    public static List<string[]> getLines(List<string> lines)
+    {
+        List<string[]> body = new List<string[]>();
+        foreach (string line in lines)
+        {
+            string[] lineItems = line.Split(',');
+            body.Add(lineItems);
+        }
+        return body;
+    }
+
+    public static List<string> getProducts(List<string[]> body)
+    {
+        List<string> products = new List<string>();
+        foreach (string[] line in body)
+        {
+            products.Add(line[0].Trim());
+        }
+        HashSet<string> newSet = new HashSet<string>(products);
+        return newSet.ToList();
+    }
+    public static string[] getHeaders(string file)
+    {
+        return file.Split('\n')[0].Split(',');
+    }
+    public static List<string> getBody(string file)
+    {
+        List<string> body = new List<string>(file.Split('\n'));
+        body.RemoveAt(0);
+        return body;
+    }
+
+    public static List<string> getOriginalYears(List<string[]> body)
+    {
+        List<string> originals = new List<string>();
+        foreach (string[] line in body)
+        {
+            originals.Add(line[1].Trim());
+        }
+        HashSet<string> newSet = new HashSet<string>(originals);
+        return newSet.ToList();
+    }
+
+    public static List<double> getValues(List<string[]> body)
+    {
+        List<double> values = new List<double>();
+        foreach (string[] line in body)
+        {
+            double value = double.Parse(line[line.Length - 1], System.Globalization.CultureInfo.InvariantCulture);
+            values.Add(value);
+        }
+        return values;
+    }
+
+    public static double getValue(List<string[]> lines, string product, string originalYear, string developement)
+    {
+        double finalValue = 0;
+
+        List<string[]> value = lines.Where(x => (x[0].Trim().Equals(product) && x[1].Trim().Equals(originalYear) && x[2].Trim().Equals(developement))).ToList();
+        if (value.Count > 0)
+        {
+            finalValue = double.Parse(value[0][3], System.Globalization.CultureInfo.InvariantCulture);
+        }
+        return finalValue;
+    }
+
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace willis_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = FileHelper.ReadFile(@"C:\Users\hp\Desktop\rania space work\willis test\test.txt");
+
+            string[] headers = FileHelper.getHeaders(file);
+            List<string> body = FileHelper.getBody(file);
+            List<string[]> lines = FileHelper.getLines(body);
+            List<string> originalYears = FileHelper.getOriginalYears(lines);
+            List<string> products = FileHelper.getProducts(lines);
+            List<double> values = FileHelper.getValues(lines);
+            // double value1 = FileHelper.getValue(lines, "Comp", "1992", "1992");
+            // double value2 = FileHelper.getValue(lines, "Non-Comp", "1992", "1992");
+
+
+            List<string[]> cumulativeLines = new List<string[]>();
+            foreach (string product in products)
+            {
+                foreach (string originalYear in originalYears)
+                {
+                    double cumulative = 0;
+
+                    for (int i = 0; i < originalYears.Count; i++)
+                    {
+                        double value = FileHelper.getValue(lines, product, originalYear, originalYears[i]);
+                        if (value > 0)
+                        {
+                            List<string> newLine = new List<string>();
+                            newLine.Add(product);
+                            newLine.Add(originalYear);
+                            cumulative = cumulative + value;
+                            newLine.Add(originalYear);
+                            newLine.Add(cumulative.ToString());
+                            cumulativeLines.Add(newLine.ToArray());
+                        }
+
+                    }
+                }
+            }
+            string[] cumulativeHeaders = headers;
+            string result = string.Join(',', cumulativeHeaders).Replace("Incremental","Cumulative");
+            foreach (string[] content in cumulativeLines)
+            {
+                string line = string.Join(',', content);
+                result = result + line + System.Environment.NewLine;
+            }
+
+            FileHelper.WriteFile(@"C:\Users\hp\Desktop\rania space work\willis test\cumulative.txt", result.Trim());
+
+
+        }
+    }
+}
